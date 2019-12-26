493039a300d5355f81c155d1ceec138e703303b0
PakkuDon <pakku.pd@gmail.com>
2019-12-26 23:56:21 +1100

    Automatically generate UUIDs for new records

--- db_create.sql
+++ db_create.sql
@@ -1,15 +1,16 @@
 DROP TABLE IF EXISTS links;
 DROP TABLE IF EXISTS redirects;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 
 CREATE TABLE links (
-  id SERIAL4 PRIMARY KEY,
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   hash VARCHAR(400),
   link TEXT,
   created_at TIMESTAMP
 );
 
 CREATE TABLE redirects (
-  id SERIAL4 PRIMARY KEY,
-  link_id SERIAL4 NOT NULL,
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  link_id uuid NOT NULL,
   visited_at TIMESTAMP
 );
