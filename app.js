a46674ddf38c6b8cb27970e34a140b91976a13bd
PakkuDon <pakku.pd@gmail.com>
2019-12-27 21:17:46 +1100

    Redirect to original URL when visiting obfuscated URL

--- app.js
+++ app.js
@@ -17,6 +17,24 @@ app.get('/', (request, response) => {
   response.send('Hello World')
 })
 
+app.get('/:id', (request, response) => {
+  const hash = request.params.id
+
+  db.query('SELECT * FROM links WHERE hash = $1', [hash])
+    .then(result => {
+      if (result.rows.length === 0) {
+        response.sendStatus(404)
+        return
+      }
+      const originalUrl = result.rows[0].link
+      response.redirect(originalUrl)
+    })
+    .catch(error => {
+      console.error(error)
+      response.sendStatus(500)
+    })
+})
+
 app.post('/api/links', (request, response) => {
   const urlToTransform = request.body.url
 
