6ec9515077fb553a1e2ef820ec38320c1126c12c
PakkuDon <pakku.pd@gmail.com>
2020-01-12 23:52:36 +1100

    Add test for GET / endpoint

--- cypress/integration/url_obfuscator_spec.js
+++ cypress/integration/url_obfuscator_spec.js
@@ -1,3 +1,12 @@
 describe('URL Obfuscator', () => {
-  // TODO
+  describe('GET /', () => {
+    it('returns API documentation', () => {
+      cy.visit('/')
+      cy.get('body')
+        .should('contain', 'GET /')
+        .should('contain', 'POST /api/links')
+        .should('contain', 'GET /:hash')
+        .should('contain', 'GET /:hash/info')
+    })
+  })
 })
