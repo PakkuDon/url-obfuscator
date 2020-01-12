e593271322695be5fe5c9b2649c1b76f08cf6803
PakkuDon <pakku.pd@gmail.com>
2020-01-12 23:56:46 +1100

    Add test for POST /api/links endpoint
    Verify that generated redirect goes to input URL

--- cypress/integration/url_obfuscator_spec.js
+++ cypress/integration/url_obfuscator_spec.js
@@ -9,4 +9,25 @@ describe('URL Obfuscator', () => {
         .should('contain', 'GET /:hash/info')
     })
   })
+
+  describe('POST /api/links', () => {
+    it('returns a JSON response', () => {
+      cy.request('POST', '/api/links', { url: 'https://example.com' })
+        .its('headers')
+        .its('content-type')
+        .should('contain', 'application/json')
+    })
+
+    it('returns a redirect to the original URL', () => {
+      cy.request('POST', '/api/links', { url: 'https://example.com' })
+        .its('body')
+        .then(response => {
+          cy.request({ url: response.new_url, followRedirect: false })
+            .then(response => {
+              expect(response.status).to.eq(302)
+              expect(response.redirectedToUrl).to.include('https://example.com')
+            })
+        })
+    })
+  })
 })
